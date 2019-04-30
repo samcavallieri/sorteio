406a40e9a46ebefc676d222909a7303ead802acd
Samuel Cavallieri <samuelcavallieri@gmail.com>
2019-04-30 19:51:38 -0300

    Create action button create labels

--- Sorteio/ViewController.swift
+++ Sorteio/ViewController.swift
@@ -15,6 +15,11 @@ class ViewController: UIViewController {
         // Do any additional setup after loading the view, typically from a nib.
     }
 
+    @IBOutlet weak var legendaResultado: UILabel!
+    @IBAction func gerarNumero(_ sender: Any) {
+    }
+    
+    
     override func didReceiveMemoryWarning() {
         super.didReceiveMemoryWarning()
         // Dispose of any resources that can be recreated.
