b0129880962f1b430717601d749355299ce96673
Samuel Cavallieri <samuelcavallieri@gmail.com>
2019-04-30 19:20:27 -0300

    Initial Commit

---
+++ Sorteio/ViewController.swift
@@ -0,0 +1,25 @@
+//
+//  ViewController.swift
+//  Sorteio
+//
+//  Created by Samuel Cavallieri on 30/04/19.
+//  Copyright © 2019 Samuel Cavallieri. All rights reserved.
+//
+
+import UIKit
+
+class ViewController: UIViewController {
+
+    override func viewDidLoad() {
+        super.viewDidLoad()
+        // Do any additional setup after loading the view, typically from a nib.
+    }
+
+    override func didReceiveMemoryWarning() {
+        super.didReceiveMemoryWarning()
+        // Dispose of any resources that can be recreated.
+    }
+
+
+}
+
